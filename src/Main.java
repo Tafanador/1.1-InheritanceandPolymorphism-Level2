54d07561786b20f1e49093b3702eeb4c4e243c36
Tafanador <tafanador13@gmail.com>
2025-09-15 11:19:41 +0200

    static and final attributes  call static and non-static methods in the main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Smartphone nokia = new Smartphone("Nokia", "B14");
+
+        nokia.call("665431209");
+        nokia.photograph();
+        nokia.alarm();
+
+}
+}
+
+
